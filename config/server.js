33e3625081d30d5d8fd7537c4c30ca33c99127eb
Thiago Santos <thiagocroza472@gmail.com>
2021-01-21 13:46:31 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc61aee7957d8cd29552b53bdbc0293e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4dfb787-3eb8-48cf-8fc0-2682ec668be8'
+};
